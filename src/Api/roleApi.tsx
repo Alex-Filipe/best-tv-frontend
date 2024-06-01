fa7114c6da45a0d31211ff889eca2d4dbc173523
Alex Filipe <alexfilipe0508@gmail.com>
2024-05-31 23:04:37 -0300

    feature: adcionando criacao de perfil na pagina home

--- src/Api/roleApi.tsx
+++ src/Api/roleApi.tsx
@@ -5,15 +5,37 @@ import axios, { AxiosResponse } from 'axios';
 const apiUrl: string | undefined = process.env.REACT_APP_LINK_API;
 
 export const fetchUserProfiles = async (userId: string, token: string): Promise<any> => {
-  try {
-    const response: AxiosResponse<any> = await axios.get(`${apiUrl}/roles/${userId}`, {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching profiles:', error);
-    throw error;
-  }
+    try {
+        const response: AxiosResponse<any> = await axios.get(`${apiUrl}/roles/${userId}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching profiles:', error);
+        throw error;
+    }
+};
+
+export const createProfile = async (token: string, newProfileName: string, user_id: number): Promise<any> => {
+    try {
+        const response: AxiosResponse<any> = await axios.post(
+            `${apiUrl}/new_role`,
+            {
+                name: newProfileName,
+                user_id: user_id
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error('Error creating profile:', error);
+        throw error;
+    }
 };
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,82 +1,79 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Box, Button, Container, TextField, Typography } from '@mui/material';
 import { useAuth } from '../../Context/AuthContext';
-import { fetchUserProfiles } from '../../Api/roleApi';
+import { createProfile, fetchUserProfiles } from '../../Api/roleApi';
 
 interface Profile {
-  id: number;
-  name: string;
+    id: number;
+    name: string;
 }
 
 const Home: React.FC = () => {
-  const { user, token } = useAuth();
-  const [profiles, setProfiles] = useState<Profile[]>([]);
-  const [newProfileName, setNewProfileName] = useState('');
+    const { user, token } = useAuth();
+    const [profiles, setProfiles] = useState<Profile[]>([]);
+    const [newProfileName, setNewProfileName] = useState('');
 
-  // GET de Perfis da conta
-  useEffect(() => {
-    const fetchProfiles = async () => {
-      if (user?.user_id && token) {
-        try {
-          const data = await fetchUserProfiles(user.user_id, token);
-          setProfiles(data);
-        } catch (error) {
-          console.error('Error fetching profiles:', error);
+    // Função para buscar perfis
+    const fetchProfiles = useCallback(async () => {
+        if (user?.user_id && token) {
+            try {
+                const data = await fetchUserProfiles(user.user_id, token);
+                setProfiles(data);
+            } catch (error) {
+                console.error('Error fetching profiles:', error);
+            }
+        } else {
+            console.warn('User ID or token is missing');
         }
-      } else {
-        console.warn('User ID or token is missing');
-      }
+    }, [user?.user_id, token]);
+
+    useEffect(() => {
+        fetchProfiles();
+    }, [token, user?.user_id, fetchProfiles]);
+
+    const handleCreateProfile = async () => {
+        if (token && newProfileName) {
+            try {
+                await createProfile(token, newProfileName, user.user_id);
+                setNewProfileName('');
+                fetchProfiles();
+            } catch (error) {
+                console.error('Error creating profile:', error);
+            }
+        }
     };
 
-    fetchProfiles();
-  }, [token, user?.user_id]);
-
-  const handleCreateProfile = async () => {
-    // const response = await fetch('/api/profiles', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'Authorization': `Bearer ${token}`
-    //   },
-    //   body: JSON.stringify({ name: newProfileName })
-    // });
-
-    // if (response.ok) {
-    //   const newProfile = await response.json();
-    //   setProfiles([...profiles, newProfile]);
-    //   setNewProfileName('');
-    // }
-  };
-
-  return (
-    <Container>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Selecione um perfil para continuar.
-      </Typography>
-      <Box mt={4}>
-        {profiles.length > 0 ? (
-          profiles.map(profile => (
-            <Box key={profile.id} mb={2}>
-              <Typography variant="h6">{profile.name}</Typography>
+    return (
+        <Container>
+            <Typography variant="h4" component="h1" gutterBottom>
+                Selecione um perfil para continuar
+            </Typography>
+            <Box mt={4}>
+                {profiles.length > 0 ? (
+                    profiles.map(profile => (
+                        <Box key={profile.id} mb={2}>
+                            <Typography variant="h6">{profile.name}</Typography>
+                        </Box>
+                    ))
+                ) : (
+                    <Typography variant="h6">Você não tem perfis. Crie um novo perfil abaixo:</Typography>
+                )}
             </Box>
-          ))
-        ) : (
-          <Typography variant="h6">Você não tem perfis. Crie um novo perfil abaixo:</Typography>
-        )}
-      </Box>
-      <Box mt={4} display="flex">
-        <TextField
-          label="Nome do Perfil"
-          value={newProfileName}
-          onChange={(e) => setNewProfileName(e.target.value)}
-          fullWidth
-        />
-        <Button onClick={handleCreateProfile} variant="contained" color="primary" sx={{ ml: 2 }}>
-          Criar Perfil
-        </Button>
-      </Box>
-    </Container>
-  );
+            {profiles.length < 4 && (
+                <Box mt={4} display="flex">
+                    <TextField
+                        label="Nome do Perfil"
+                        value={newProfileName}
+                        onChange={(e) => setNewProfileName(e.target.value)}
+                        fullWidth
+                    />
+                    <Button onClick={handleCreateProfile} variant="contained" color="primary" sx={{ ml: 2 }}>
+                        Criar Perfil
+                    </Button>
+                </Box>
+            )}
+        </Container>
+    );
 };
 
 export default Home;
